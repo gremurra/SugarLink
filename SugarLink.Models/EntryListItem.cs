b3e01feb07d68ce8431988b48aed0a16a9a6d874
Seth McLaughlin <Seth.d.mclaughlin@gmail.com>
2020-04-24 17:21:39 -0400

    Added Classes EntryCreate, EntryListItem.

---
+++ SugarLink.Models/EntryListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarLink.Models
+{
+    public class EntryListItem
+    {
+        public int EntryId { get; set; }
+        
+        [DisplayName("Date of entry")]
+        public DateTimeOffset EntryDate { get; set; }
+        
+        [DisplayName("Blood sugar level")]
+        public int BloodSugarReading { get; set; }
+    }
+}
